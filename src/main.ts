a90e0e822230e134c195f992a1aeed0af0266cf7
Marcus Aguiar <marcusaguiardev@gmail.com>
2022-03-04 11:11:51 -0300

    first commit

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+/**
+ * Main file
+ */
+
+import Phaser from 'phaser'
+import RoomScene from './scenes/room.scene'
+
+const gameConfig: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 0 }
+        }
+    },
+    scene: [RoomScene]
+}
+
+export default new Phaser.Game(gameConfig)
