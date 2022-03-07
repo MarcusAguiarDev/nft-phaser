89db8d50e1bb166f1abcd2b5fc401d6ff052a59c
Marcus Aguiar <marcusaguiardev@gmail.com>
2022-03-07 12:46:40 -0300

    inventory ui component

--- src/dom-components/ui-components.jsx
+++ src/dom-components/ui-components.jsx
@@ -1,18 +1,47 @@
-export const quickMenu = (
-    <section id="quick-menu" class="quick-menu">
-        <QuickMenuItem
-            text="Inventory"
-            icon={<i class="fa fa-solid fa-box-open"></i>} />
-        <QuickMenuItem
-            text="Settings"
-            icon={<i class="fa fa-solid fa-gear"></i>} />
-    </section>
-)
-
-function QuickMenuItem({ icon, text }) {
+export function QuickMenu() {
     return (
-        <div class="quick-menu-item" data-content={text}>
+        <section id="quick-menu" class="quick-menu">
+            <QuickMenuItem
+                id="quick-menu-inventory"
+                text="Inventory"
+                icon={<i class="fa fa-solid fa-box-open"></i>} />
+            <QuickMenuItem
+                id="quick-menu-settings"
+                text="Settings"
+                icon={<i class="fa fa-solid fa-gear"></i>} />
+        </section>
+    )
+}
+
+export function QuickMenuItem({ id, icon, text }) {
+    return (
+        <div id={id} class="quick-menu-item" data-content={text}>
             {icon}
+        </div>
+    )
+}
+
+export function InventoryMenu(items) {
+    return (
+        <section class="inventory-menu">
+            {items.map(item => {
+                <InventoryMenuItem
+                    title={item.title}
+                    nftNumber={item.nftNumber}
+                    image={item.image} />
+            })}
+        </section>
+    )
+}
+
+export function InventoryMenuItem({ title, nftNumber, image }) {
+    return (
+        <div class="inventory-menu-item">
+            <div class="inventory-menu-item__title">{title}</div>
+            <div class="inventory-menu-item__nft">#{nftNumber}</div>
+            <div class="inventory-menu-item__icon">
+
+            </div>
         </div>
     )
 }
--- src/services/GameUi.ts
+++ src/services/GameUi.ts
@@ -3,9 +3,13 @@
  * responsible for the menu interactions to open other features, setup configurations, etc
  */
 
-import { quickMenu } from "~/dom-components/ui-components"
+import { InventoryMenu, QuickMenu } from "~/dom-components/ui-components"
+
 
 const QUICK_MENU_ID = 'quick-menu'
+const INVENTORY_MENU_ID = 'inventory-menu'
+const QUICK_MENU_INVENTORY_ID = "quick-menu-inventory"
+const QUICK_MENU_SETTINGS_ID = "quick-menu-settings"
 
 export default class GameUi {
 
@@ -15,18 +19,45 @@ export default class GameUi {
 
     timers = new Array<number>()
 
+    quickMenuInventoryDom!: HTMLDivElement
+    quickMenuSettingsDom!: HTMLDivElement
+
     constructor(scene: Phaser.Scene) {
         this.scene = scene
-
         this.createQuickMenu()
+    }
+
+    createInventoryMenu(items: Array<any>) {
+        const inventoryMenuElement = this.scene.add.dom(0, 580, InventoryMenu(items))
+            .setOrigin(0, 1)
+            .setVisible(true)
+            .setScrollFactor(0)
+        const inventoryMenuDom = document.getElementById(INVENTORY_MENU_ID) as HTMLInputElement
+
     }
 
     createQuickMenu() {
         //create quickMenu phaser element and dom
-        this.quickMenuElement = this.scene.add.dom(760, 580, quickMenu)
+        this.quickMenuElement = this.scene.add.dom(760, 580, QuickMenu())
             .setOrigin(1, 1)
             .setVisible(true)
             .setScrollFactor(0)
         this.quickMenuDom = document.getElementById(QUICK_MENU_ID) as HTMLInputElement
+
+        this.quickMenuInventoryDom = this.quickMenuDom.querySelector(`#${QUICK_MENU_INVENTORY_ID}`) as HTMLDivElement
+        this.addInventoryListener()
+    }
+
+    addInventoryListener() {        
+        this.quickMenuInventoryDom.onclick = e => {
+            const items = this.getAccountItems()
+            this.createInventoryMenu(items)
+            this.quickMenuElement.destroy()
+        }
+    }
+    
+    getAccountItems(): Array<any>{
+        //TODO  
+        return []
     }
 }
